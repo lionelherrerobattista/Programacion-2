12e94ea54f6f0602387480ad7ec279538cae670c
Lionel <lla@lala.com>
2019-03-20 20:44:21 -0300

    Clase 1

---
+++ Clase_01/Clase_01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    class Ejercicio_01
+    {
+        static void Main(string[] args)
+        {
+            //Llevan valores iniciales:
+            int maximo = int.MinValue; //Le asigno el valor mínimo
+            int minimo = int.MaxValue; //Le asigno el valor máximo
+            int i=0;
+            int acumuladorNumeros = 0;
+            float promedio;
+            int numeroAuxiliar;
+        
+
+            Console.Title = "Ejercicio Nro 01";
+
+            Console.WriteLine("Ingrese 5 números:");
+            do
+            {
+                if (int.TryParse(Console.ReadLine(), out numeroAuxiliar)) //devuelve bool
+                {
+                    acumuladorNumeros += numeroAuxiliar;
+
+                    if (numeroAuxiliar > maximo)
+                    {
+                        maximo = numeroAuxiliar;
+                    }
+
+                    if (numeroAuxiliar < minimo)
+                    {
+                        minimo = numeroAuxiliar;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+
+                i++;
+
+            } while (i < 5);
+
+            //Calculo el promedio
+            promedio = (float)acumuladorNumeros / 5;
+
+            Console.WriteLine("Promedio {0}", promedio);
+            Console.WriteLine("Mayor {0}", maximo);
+            Console.WriteLine("Menor {0}", minimo);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Clase_01/Ejercicio2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Ejercicio_02
+    {
+        static void Main(string[] args)
+        {
+            double numeroUsuario = 0;
+            double numeroAuxiliar;
+          
+        
+
+            Console.Title = "Ejercicio Nro 02";
+
+            
+            do
+            {
+                Console.Write("Ingrese un numero: ");
+
+                if (double.TryParse(Console.ReadLine(), out numeroAuxiliar)) //devuelve bool
+                {
+                    numeroUsuario = numeroAuxiliar;
+                }
+                
+                if(numeroUsuario < 0)
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar número!");
+                }
+
+                
+           
+            } while (numeroUsuario < 0);
+
+                Console.WriteLine("Al cuadrado: {0}", Math.Pow(numeroUsuario, 2));
+                Console.WriteLine("Al cubo: {0}", Math.Pow(numeroUsuario, 3));
+            
+
+
+            Console.ReadKey();
+
+                
+
+        }
+    }
+}
