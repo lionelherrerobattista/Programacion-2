ac86ddd1a9a843443998210ee3b46590b2277830
Lionel <lla@lala.com>
2019-03-20 22:14:06 -0300

    Clase 1

---
+++ Clase_01/Clase_05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_05
+{
+    class Clase_05
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Title = "Ejercicio Nro 05";
+        }
+    }
+}
