dc9a7ef8cff1bf498f3a19b01669651ec6ee6335
Lionel Herrero Battista <->
2019-03-27 16:41:39 -0300

    Métodos Estáticos

---
+++ Ejercicios_Guia/Metodos_Estaticos/Ejercicio_11/Program.cs
@@ -0,0 +1,111 @@
+﻿/* 11. Ingresar 10 números enteros que pueden estar dentro de un rango
+ * de entre -100 y 100. Para ello realizar una clase llamada Validacion
+ * que posea un método estático llamado Validar,
+ * que posea la siguiente firma: bool Validar(int valor, int min, int max):
+ * a. valor: dato a validar
+ * b. min y max: rango en el cual deberá estar la variable valor.
+ * Terminado el ingreso mostrar el valor mínimo, el valor máximo
+ * y el promedio. Nota: Utilizar variables escalares,
+ * NO utilizar vectores. 
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_11
+{
+    class Validacion
+    {
+        public static bool Validar(int valor, int min, int max)
+        {
+            bool valido = false;
+
+            //Compruebo que el número se encuentre dentro del rango
+            if (valor <= max && valor >= min)
+            {
+                valido = true; //valido el número
+            }
+
+            return valido;
+
+        }
+
+    }
+
+    class Ejercicio_11
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string numeroAuxiliar;
+            int valor;
+            int minimo = Int32.MaxValue;
+            int maximo = Int32.MinValue;
+            float promedio;
+            int acumuladorNumeros = 0;
+            bool validado;
+
+            Console.Title = "Ejercicio Nro 11";
+
+            Console.WriteLine("Ingrese 10 números: ");
+            
+
+            //Pido los 10 números
+            for(i = 0; i < 10; i++)
+            {
+                do
+                {
+                    Console.Write("Número {0}: ", i + 1);
+                    numeroAuxiliar = Console.ReadLine();
+                    
+                    //Todavía el número no está validado:
+                    validado = false;    
+                    
+                    if (Int32.TryParse(numeroAuxiliar, out valor))
+                    {
+                        //valido el número
+                        validado = Validacion.Validar(valor, -100, 100);
+                    }
+
+                    //Si no lo pudo validar
+                    if(validado == false)
+                    {
+                        Console.WriteLine("Error. Fuera de rango"
+                            + " Ingrese el número nuevamente.");
+                    }
+                    else //si pudo validar
+                    {
+                        //voy sumando los números:
+                        acumuladorNumeros += valor;
+
+                        //compruebo si es menor o mayor
+                        if(valor > maximo)
+                        {
+                            maximo = valor;
+
+                        }
+
+                        if(valor < minimo)
+                        {
+                            minimo = valor;
+                        }
+                    }
+
+                } while (validado == false);
+
+            }//fin del for
+
+            //calculo el promedio
+            promedio = (float)acumuladorNumeros / 10;
+
+            //Muestro los resultados
+            Console.WriteLine("\nMáximo: {0} \nMínimo:{1}"
+                + "\nPromedio:{2,3}", maximo, minimo, promedio);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicios_Guia/Metodos_Estaticos/Ejercicio_12/Program.cs
@@ -0,0 +1,73 @@
+﻿/*12. Realizar un programa que sume números enteros
+ * hasta que el usuario lo determine,
+ * por medio de un mensaje "¿Continuar? (S/N)".
+ * En el método estático ValidaS_N(char c)
+ * de la clase ValidarRespuesta, se validará el ingreso de opciones.
+ * El método devolverá un valor de tipo booleano,
+ * TRUE si se ingresó una 'S' y FALSE si se ingresó cualquier otro valor.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_12
+{
+    class ValidarRespuesta
+    {
+        public static bool ValidarS_N(char c)
+        {
+            bool valido = false;
+
+            //Comparo si el usuario ingresó una s
+            if(Char.ToUpper(c) == 'S')
+            {
+                valido = true;
+            }
+
+            return valido;
+        }
+    }
+
+    class Ejercicio_12
+    {
+        static void Main(string[] args)
+        {
+            string numeroAuxiliar;
+            int numero;
+            int suma = 0;
+            char caracter;
+            bool respuesta;
+
+            Console.Title = "Ejercicio Nro 12";
+
+            Console.WriteLine("Ingrese los numeros a sumar:");
+
+            do
+            {
+                //Pido el número a sumar
+                Console.Write("Numero: ");
+                numeroAuxiliar = Console.ReadLine();
+
+                if(Int32.TryParse(numeroAuxiliar, out numero))
+                {
+
+                }
+
+                //Calculo y muestro el resultado
+                suma += numero;
+                Console.WriteLine("Resultado: {0}", suma);
+
+                //Pregunto al usuario si desea seguir sumando
+                Console.Write("\n¿Continuar? (S/N): ");
+                caracter = Console.ReadKey().KeyChar;
+                Console.WriteLine("\n");
+
+                //Valido la respuesta
+                respuesta = ValidarRespuesta.ValidarS_N(caracter);
+
+            } while (respuesta == true);
+        }
+    }
+}
