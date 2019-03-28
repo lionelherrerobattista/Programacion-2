5f2b749acf500c2cc7fc64841f3d16567f95d299
Lionel Herrero Battista <->
2019-03-28 16:20:19 -0300

    Objetos

---
+++ Ejercicios_Guia/Objetos/Ejercicio_16/Alumno.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Alumno
+    {
+        private byte nota1;
+        private byte nota2;
+        private float notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        public void CalcularFinal()
+        {
+            if(nota1 >= 4 && nota2 >= 4)
+            {
+                Random r = new Random();
+
+                notaFinal = r.Next(1, 11);
+            }
+            else
+            {
+                notaFinal = -1;
+            }
+
+        }
+
+        public void Estudiar(byte notaUno, byte notaDos)
+        {
+            nota1 = notaUno;
+            nota2 = notaDos;
+        }
+
+        public string Mostrar()
+        {
+            string datosAlumno;
+
+            datosAlumno = "Nombre: " + nombre + "\nApellido: " + apellido
+                + "\nLegajo: " + legajo.ToString() + "\nNota 1: " + nota1.ToString()
+                + "\nNota 2: " + nota2.ToString();
+
+            if(notaFinal != -1)
+            {
+                datosAlumno += "\nNota Final: " + notaFinal.ToString();
+            }
+            else
+            {
+                datosAlumno += "\nNota Final: Alumno desaprobado.";
+            }
+
+            return datosAlumno;
+        }
+
+    }
+}
---
+++ Ejercicios_Guia/Objetos/Ejercicio_16/Ejercicio_16.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_16
+{
+    class Ejercicio_16
+    {
+        static void Main(string[] args)
+        {
+            string auxiliarString;
+            int auxiliarNumero;
+            byte notaUno;
+            byte notaDos;
+
+            Console.Title = "Ejercicio Nro 16";
+
+            Alumno alumnoUno = new Alumno();
+            Alumno alumnoDos = new Alumno();
+            Alumno alumnoTres = new Alumno();
+
+            //Ingreso datos alumnoUno
+            Console.WriteLine("Alumno 1:");
+
+            Console.Write("Nombre: ");
+            alumnoUno.nombre = Console.ReadLine();
+
+            Console.Write("Apellido: ");
+            alumnoUno.apellido = Console.ReadLine();
+
+            Console.Write("Legajo: ");
+            auxiliarString = Console.ReadLine();
+            if(Int32.TryParse(auxiliarString, out auxiliarNumero))
+            {
+                alumnoUno.legajo = auxiliarNumero;
+            }
+
+            Console.Write("Nota 1: ");
+            auxiliarString = Console.ReadLine();
+            if (Byte.TryParse(auxiliarString, out notaUno))
+            {
+                
+            }
+
+            Console.Write("Nota 2: ");
+            auxiliarString = Console.ReadLine();
+            if (Byte.TryParse(auxiliarString, out notaDos))
+            {
+
+            }
+
+            alumnoUno.Estudiar(notaUno, notaDos);
+            alumnoUno.CalcularFinal();
+            Console.WriteLine("\nDatos alumno:\n{0}", alumnoUno.Mostrar());
+
+            //Ingreso datos alumnoDos
+            Console.WriteLine("\nAlumno 2:");
+
+            Console.Write("Nombre: ");
+            alumnoDos.nombre = Console.ReadLine();
+
+            Console.Write("Apellido: ");
+            alumnoDos.apellido = Console.ReadLine();
+
+            Console.Write("Legajo: ");
+            auxiliarString = Console.ReadLine();
+            if (Int32.TryParse(auxiliarString, out auxiliarNumero))
+            {
+                alumnoDos.legajo = auxiliarNumero;
+            }
+
+            Console.Write("Nota 1: ");
+            auxiliarString = Console.ReadLine();
+            if (Byte.TryParse(auxiliarString, out notaUno))
+            {
+
+            }
+
+            Console.Write("Nota 2: ");
+            auxiliarString = Console.ReadLine();
+            if (Byte.TryParse(auxiliarString, out notaDos))
+            {
+
+            }
+
+            alumnoDos.Estudiar(notaUno, notaDos);
+            alumnoDos.CalcularFinal();
+            Console.WriteLine("\nDatos alumno:\n{0}", alumnoDos.Mostrar());
+
+
+            //Ingreso datos alumnoTres
+            Console.WriteLine("\nAlumno 3:");
+
+            Console.Write("Nombre: ");
+            alumnoTres.nombre = Console.ReadLine();
+
+            Console.Write("Apellido: ");
+            alumnoTres.apellido = Console.ReadLine();
+
+            Console.Write("Legajo: ");
+            auxiliarString = Console.ReadLine();
+            if (Int32.TryParse(auxiliarString, out auxiliarNumero))
+            {
+                alumnoTres.legajo = auxiliarNumero;
+            }
+
+            Console.Write("Nota 1: ");
+            auxiliarString = Console.ReadLine();
+            if (Byte.TryParse(auxiliarString, out notaUno))
+            {
+
+            }
+
+            Console.Write("Nota 2: ");
+            auxiliarString = Console.ReadLine();
+            if (Byte.TryParse(auxiliarString, out notaDos))
+            {
+
+            }
+
+            alumnoTres.Estudiar(notaUno, notaDos);
+            alumnoTres.CalcularFinal();
+            Console.WriteLine("\nDatos alumno:\n{0}", alumnoTres.Mostrar());
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
