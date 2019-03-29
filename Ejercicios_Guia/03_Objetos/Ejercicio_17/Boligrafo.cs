ec2d51170d2b03b95fe317451138e307dd104065
Lionel <lalala@lala.com>
2019-03-28 22:04:15 -0300

    Objetos

---
+++ Ejercicios_Guia/03_Objetos/Ejercicio_17/Boligrafo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_17
+{
+    class Boligrafo
+    {
+        const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+            this.tinta = tinta;
+            this.color = color;
+        }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="gasto"></param>
+        /// <param name="dibujo"></param>
+        /// <returns></returns>
+        public bool Pintar(int gasto, out string dibujo)
+        {
+            int i;
+            short auxiliarTinta;
+            bool pinto = false;
+
+            if(this.tinta > 0)
+            {
+                pinto = true;
+
+                auxiliarTinta = (short)(0 - gasto);
+
+                //resto
+                this.SetTinta(auxiliarTinta);
+
+                for(i = this.tinta; i >= 0; i--)
+                {
+                    
+                }
+
+            }
+
+
+
+
+
+
+            return pinto;
+        }
+
+        public void Recargar()
+        {
+            short recarga;
+            
+            recarga = (short)(cantidadTintaMaxima - this.tinta);//Castear toda la cuenta
+            
+            //reutilizo código
+            this.SetTinta(recarga);
+        }
+
+        private void SetTinta(short tinta)
+        {
+            short auxiliarTinta;
+
+            auxiliarTinta = this.tinta;
+
+            if(tinta > 0)//cargo tinta
+            {
+        
+                auxiliarTinta += tinta;
+            }
+            else if(tinta < 0)//gasto tinta
+            {
+                auxiliarTinta -= tinta;
+            }
+
+            //compruebo si no me paso
+            if(auxiliarTinta >= 0 && auxiliarTinta <= cantidadTintaMaxima)
+            {
+                this.tinta = auxiliarTinta;
+            }
+
+        }
+
+    }
+}
---
+++ Ejercicios_Guia/03_Objetos/Ejercicio_17/Ejercicio_17.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_17
+{
+    class Ejercicio_17
+    {
+        static void Main(string[] args)
+        {
+            short tintaBoligrafo;
+
+            tintaBoligrafo = 100;
+
+            Boligrafo boligrafoAzul = new Boligrafo(tintaBoligrafo, ConsoleColor.Blue);
+
+            tintaBoligrafo = 50;
+
+            Boligrafo boligrafoRojo = new Boligrafo(tintaBoligrafo, ConsoleColor.Red);
+
+            Console.Title = "Ejercicio Nro 17";
+        }
+    }
+}
