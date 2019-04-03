0deb09c09cb61c5647e5228ad5405a99a2a809f5
Lionel <lala@lalal.com>
2019-04-03 20:50:09 -0300

    Repaso

---
+++ Clases/Clase_05/Repaso/Repaso/Estante.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion): this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+
+        public static string MostrarEstante(Estante e)
+        {
+            int i;
+            string informacion;
+            //estante
+            informacion = String.Format("Ubicación estante: {0}\n", e.ubicacionEstante);
+            
+            //detalle de cada uno de los productos
+            for (i = 0; i < e.productos.Length; i++)
+            {
+                if(!(Object.Equals(e.productos[i], null)))
+                {
+                    informacion += Producto.MostrarProducto(e.productos[i]);
+                }
+                
+            }
+
+            
+            return informacion;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            int i;
+
+            Estante estanteAuxiliar = new Estante(e.productos.Length);
+
+            if (e == p)//si el producto está en el estante
+            {
+                estanteAuxiliar = e;
+
+                for (i = 0; i < e.productos.Length-1; i++)
+                {
+                    if(estanteAuxiliar.productos[i] == p)
+                    {
+                        estanteAuxiliar.productos[i] = null;//Le saco la referencia
+                        break;
+                    }
+                }
+ 
+            }
+
+            return estanteAuxiliar;
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            int i;
+            bool agrego = false;
+
+            if(e != p)//si no está el producto en el estante
+            {
+                for(i = 0; i < e.productos.Length; i++)
+                {
+                    if(Object.Equals(e.productos[i], null))
+                    {
+                        e.productos[i] = p;
+                        agrego = true;
+                        break;
+                    }
+                }
+            }
+
+            return agrego;
+
+        }
+
+        public static bool operator !=(Estante e, Producto p)
+        {
+            bool noEsta = true;
+            int i;
+
+            for(i = 0; i < e.productos.Length; i++)
+            {
+                if (Object.Equals(e.productos[i], p))
+                {
+                    noEsta = false;
+                    break;
+                }
+            }
+
+            return noEsta;
+
+        }
+
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool esta = false;
+            int i;
+
+            for (i = 0; i < e.productos.Length; i++)
+            {
+                if (Object.Equals(e.productos[i], p))
+                {
+                    esta = true;
+                    break;
+                }
+            }
+
+            return esta;
+
+        }
+
+
+    }
+}
---
+++ Clases/Clase_05/Repaso/Repaso/Producto.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso
+{
+    class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            return String.Format("Codigo de Barra: {0}\nMarca: {1}\nPrecio:{2}\n", p.codigoDeBarra, p.marca, p.precio);
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            bool distintaMarca = false;
+
+            if (p.marca != marca)
+            {
+                distintaMarca = true;
+            }
+
+            return distintaMarca;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            bool distintoProducto = false;
+
+            if(p1.marca != p2.marca && p1.codigoDeBarra != p2.codigoDeBarra)
+            {
+                distintoProducto = true;
+            }
+
+            return distintoProducto;
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            bool mismaMarca = false;
+
+            if(p.marca == marca)
+            {
+                mismaMarca = true;
+            }
+
+            return mismaMarca;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool mismoProducto = false;
+
+            if(p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra)
+            {
+                mismoProducto = true;
+            }
+
+            return mismoProducto;
+        }
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+    }
+}
