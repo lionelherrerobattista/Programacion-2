689264605e0d3575a92c66504c9abe2d0220dd75
Lionel <lionherrero@gmail.com>
2019-04-11 22:07:23 -0300

    Arrays y colecciones

---
+++ Ejercicios_Guia/06_Arrays_y_Colecciones/Ejercicio_26/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int [20];
+            int numero = 0;
+            int i;
+            int j;
+
+            Random random = new Random();
+
+            for(i = 0; i < 20; i++)
+            {
+                do
+                {
+                    numero = random.Next(-100, 100);
+
+                    if (numero != 0)
+                        numeros[i] = numero;
+
+                } while (numero != 0);
+               
+            }
+
+            for (i = 0; i < 20; i++)
+            {
+                Console.WriteLine("{0}", numeros[i]);
+            }
+
+            Console.WriteLine("\n");
+
+            //Ordeno positivos
+            //
+            for (i = 0; i < numeros.Length-1; i++)
+            {
+                    for (j = i+1; j < numeros.Length ; j++)
+                    {
+                        if (numeros[i] < numeros[j] && numeros[j] > 0)
+                        {
+                            numero = numeros[i];
+                            numeros[i] = numeros[j];
+                            numeros[j] = numero;
+                        }
+
+                    }
+
+            }
+
+            Console.WriteLine("Positivos ordenados");
+
+            for (i = 0; i < 20; i++)
+            {
+                Console.WriteLine("{0}", numeros[i]);
+            }
+
+            Console.WriteLine();
+
+            //Ordeno negativos
+            //
+
+            for (i = 0; i < numeros.Length - 1; i++)
+            {
+                if (numeros[i] > 0)
+                {
+                    continue;
+                }
+                for (j = i + 1; j < numeros.Length; j++)
+                {
+                    if (numeros[i] > numeros[j] && numeros[j] < 0)
+                    {
+
+                        numero = numeros[i];
+                        numeros[i] = numeros[j];
+                        numeros[j] = numero;
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("Negativos ordenados");
+
+            for (i = 0; i < 20; i++)
+            {
+                Console.WriteLine("{0}", numeros[i]);
+            }
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ejercicios_Guia/06_Arrays_y_Colecciones/Ejercicio_27/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ejercicio_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int numero = 0;
+            Stack<int> numeros;
+
+            numeros = new Stack<int>();
+
+            Random random = new Random();
+
+            for (i = 0; i < 20; i++)
+            {
+                do
+                {
+                    numero = random.Next(-100, 100);
+
+                    if (numero != 0)
+                        numeros.Push(numero);
+
+                } while (numero != 0);
+            }
+
+            foreach (int num in numeros)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
+
+        
+    
+
