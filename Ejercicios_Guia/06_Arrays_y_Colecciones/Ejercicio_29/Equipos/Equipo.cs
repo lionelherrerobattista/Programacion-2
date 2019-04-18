ed065472f887ef1ab67d105e800741a363d009e0
Lionel <lionherrero@gmail.com>
2019-04-17 22:02:43 -0300

    Colecciones

---
+++ Ejercicios_Guia/06_Arrays_y_Colecciones/Ejercicio_29/Equipos/Equipo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equipos
+{
+    public class Equipo
+    {
+        private short cantidadDeJugadores;
+        List<Jugador> jugadores;
+        string nombres;
+
+    }
+}
---
+++ Ejercicios_Guia/06_Arrays_y_Colecciones/Ejercicio_29/Equipos/Jugador.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equipos
+{
+    class Jugador
+    {
+        private int dni;
+        private string nombre;
+        private int partidosJugados;
+        private float promedioGoles;
+        private int totalGoles;
+
+        /// <summary>
+        /// Calcula el promedio de goles del jugador y lo devuelve
+        /// </summary>
+        /// <returns>Promedio de goles del jugador</returns>
+        public float GetPromedioGoles()
+        {
+            float promedioGoles;
+
+            promedioGoles = totalGoles / partidosJugados;
+
+            return promedioGoles;
+        }
+
+        /// <summary>
+        /// Constructor por defecto del jugador, inicializa
+        /// los datos estadísticos en 0
+        /// </summary>
+        private Jugador()
+        {
+            this.promedioGoles = 0;
+            this.partidosJugados = 0;
+            this.totalGoles = 0;
+
+        }
+
+        /// <summary>
+        /// Constructor de la clase Jugador
+        /// Recibe datos personales
+        /// </summary>
+        /// <param name="dni">Dni del jugador</param>
+        /// <param name="nombre">Nombre del jugador</param>
+        public Jugador(int dni, string nombre)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+
+        }
+
+        /// <summary>
+        /// Constructor de la clase jugador
+        /// Recibe dni, nombre y datos estadísticos
+        /// </summary>
+        /// <param name="dni">DNI del jugador</param>
+        /// <param name="nombre">Nombre del jugador</param>
+        /// <param name="totalGoles">Total de goles que hizo el jugador</param>
+        /// <param name="totalPartidos">Total de partidos que jugó</param>
+        public Jugador(int dni, string nombre, int totalGoles, int totalPartidos): this(dni, nombre)
+        {
+            this.totalGoles = totalGoles;
+            this.partidosJugados = totalPartidos;
+        }
+
+        /// <summary>
+        /// Devuelve un string con los datos estadísticos del jugador
+        /// </summary>
+        /// <returns>String con los datos estadísticos</returns>
+        public string MostrarDatos()
+        {
+            string datos;
+
+            datos = string.Format("{0} {1} {2} {3} {4}", this.dni,
+                this.nombre, this.partidosJugados, this.totalGoles, this.promedioGoles);
+
+            return datos;
+        }
+
+        /// <summary>
+        /// Sobrecarga el operador != para comparar jugadores por DNI
+        /// </summary>
+        /// <param name="j1">Primer jugador a comparar</param>
+        /// <param name="j2">Segundo jugador a comparar</param>
+        /// <returns>True si los juga</returns>
+        private static bool operator !=(Jugador j1, Jugador j2)
+        {
+            bool sonDistintos = false;
+
+            if(j1.dni != j2.dni)
+            {
+                sonDistintos = true;
+            }
+
+            return sonDistintos;
+        }
+
+        private static bool operator ==(Jugador j1, Jugador j2)
+        {
+            return !(j1 != j2);
+        }
+
+
+    }
+}
