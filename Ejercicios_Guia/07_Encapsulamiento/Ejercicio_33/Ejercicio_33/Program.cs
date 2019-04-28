591bfdf8d864d6440362d8d9e192163bf3f0652e
Lionel Herrero Battista <->
2019-04-28 19:04:39 -0300

    Encapsulamiento

---
+++ Ejercicios_Guia/07_Encapsulamiento/Ejercicio_33/Ejercicio_33/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades_Ejercicio_33;
+
+namespace Ejercicio_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            Console.Title = "Ejercicio_33";
+
+            Libro nuevoLibro = new Libro();
+
+            //Agrego páginas
+            nuevoLibro[0] = "Hola";
+            nuevoLibro[1] = "Mundo";
+
+            Console.Write("Posición 1: ");
+            Console.WriteLine(nuevoLibro[1]);
+
+            //sobreescribo
+            Console.WriteLine("Sobreescribo posición 1...");
+            nuevoLibro[1] = "Juan";
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0}: {1}",i, nuevoLibro[i]);
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ejercicios_Guia/07_Encapsulamiento/Ejercicio_33/Entidades_Ejercicio_33/Libro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_Ejercicio_33
+{
+    public class Libro
+    {
+        private List<string> paginas;
+
+        public string this[int i]
+        {
+            get
+            {
+                string pagina = "";
+
+                if ( i >= 0 && i < this.paginas.Count)
+                {
+                    pagina = this.paginas[i];
+                }
+
+                return pagina;
+            }
+            set
+            {
+                if (i >= 0 && i < this.paginas.Count)
+                {
+                    this.paginas[i] = value;
+                }
+                else
+                {
+                    this.paginas.Add(value);
+                }
+
+            }
+
+
+        }
+
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+            
+          
+
+
+
+
+    }
+}
