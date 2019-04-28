de878a801ca953bacd0f5f3431d56d8648fdebf9
Lionel Herrero Battista <->
2019-04-28 18:10:13 -0300

    Encapsulamiento

---
+++ Ejercicios_Guia/07_Encapsulamiento/Ejercicio_32/Entidades/Equipo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Equipo
+    {
+
+        private short cantidadDeJugadores;
+        List<Jugador> jugadores;
+        string nombre;
+
+        private Equipo()
+        {
+            jugadores = new List<Jugador>();
+
+        }
+
+        public Equipo(short cantidad, string nombre)
+            : this()
+        {
+
+            this.cantidadDeJugadores = cantidad;
+            this.nombre = nombre;
+        }
+
+        public static bool operator +(Equipo e, Jugador j)
+        {
+            bool agregoJugador = false;
+
+            if (e.jugadores.Count < e.cantidadDeJugadores && e.jugadores.Contains(j) == false)
+            {
+                e.jugadores.Add(j);
+                agregoJugador = true;
+
+            }
+
+            return agregoJugador;
+
+
+        }
+    }
+}
