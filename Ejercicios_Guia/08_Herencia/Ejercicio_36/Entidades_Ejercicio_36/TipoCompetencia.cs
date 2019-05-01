2b286d4d9c70e927a96bd6ad9673ca67c7c94982
Lionel Herrero Battista <->
2019-05-01 19:01:39 -0300

    Herencia

---
+++ Ejercicios_Guia/08_Herencia/Ejercicio_36/Entidades_Ejercicio_36/TipoCompetencia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_Ejercicio_36
+{
+    public enum TipoCompetencia
+    {
+        F1,
+        MotoCross
+    }
+}
