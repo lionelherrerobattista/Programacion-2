d92f9dc053825cf97c285dc8e9763bfc4d2305b8
Lionel Herrero Battista <->
2019-05-04 15:09:42 -0300

    Herencia

---
+++ Ejercicios_Guia/09_Sobrecarga_Polimorfismo/Ejercicio_38/Entidades_Ejercicio_38/Sobreescrito.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_Ejercicio_38
+{
+    public class Sobreescrito : Object
+    {
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobreescrito!";
+        }
+
+        
+        public override bool Equals(object obj)
+        {
+            bool sonIguales = false;
+
+            if (obj is Sobreescrito)
+            {
+                sonIguales = true;
+            }
+
+            return sonIguales;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
---
+++ Ejercicios_Guia/09_Sobrecarga_Polimorfismo/Ejercicio_39/Entidades_Ejercicio_39/Sobreescrito.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_Ejercicio_39
+{
+    public abstract class Sobreescrito
+    {
+        protected string miAtributo;
+
+        public abstract string MiPropiedad
+        {
+            get;
+        }
+
+        public Sobreescrito()
+        {
+            this.miAtributo = "Probar abstractos";
+        }
+
+        public abstract string MiMetodo();
+
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobreescrito!";
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            bool sonIguales = false;
+
+            if (obj is Sobreescrito)
+            {
+                sonIguales = true;
+            }
+
+            return sonIguales;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
