8234432ffea73b6bed95795d4297155c8948c254
Lionel Herrero Battista <->
2019-05-04 12:05:23 -0300

    Herencia

---
+++ Ejercicios_Guia/08_Herencia/CentralTelefonica/CentralitaHerencia/Centralita.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Centralita
+    {
+        
+        private List<Llamada> listaDeLlamadas;
+        protected string razonSocial;
+
+        public float GananciasPorLocal
+        {
+            get
+            {
+                
+                return this.CalcularGanancia(TipoLlamada.Local);
+
+            }
+        }
+
+        public float GananciasPorProvincial
+        {
+            get
+            {
+                return this.CalcularGanancia(TipoLlamada.Provincial);
+            }
+        }
+
+        public float GananciasPorTotal
+        {
+            get
+            {
+                return this.CalcularGanancia(TipoLlamada.Todas);
+
+            }
+        }
+
+        public List<Llamada> Llamadas
+        {
+            get
+            {
+                return this.listaDeLlamadas;
+                
+            }
+        }
+
+        public Centralita()
+        {
+            this.listaDeLlamadas = new List<Llamada>();
+
+        }
+
+        public Centralita(string nombreEmpresa) : this()
+        {
+            this.razonSocial = nombreEmpresa;
+        }
+
+        private float CalcularGanancia(TipoLlamada tipo)
+        {
+            int i;
+            Local auxiliarLocal;
+            Provincial auxiliarProvincial;
+            float recaudado = 0;
+
+            if(tipo == TipoLlamada.Local)
+            {
+                for(i = 0; i < this.Llamadas.Count; i++)
+                {
+                    if(this.Llamadas[i] is Local)
+                    {
+                        auxiliarLocal = (Local)this.Llamadas[i];
+
+                        recaudado += auxiliarLocal.CostoLlamada;
+                    }
+                    
+                }
+            }
+            else if(tipo == TipoLlamada.Provincial)
+            {
+                for (i = 0; i < this.Llamadas.Count; i++)
+                {
+                    if (this.Llamadas[i] is Provincial)
+                    {
+                        auxiliarProvincial = (Provincial)this.Llamadas[i];
+
+                        recaudado += auxiliarProvincial.CostoLlamada;
+                    }
+
+                }
+            }
+            else if(tipo == TipoLlamada.Todas)
+            {
+                for (i = 0; i < this.Llamadas.Count; i++)
+                {
+                    if (this.Llamadas[i] is Local)
+                    {
+                        auxiliarLocal = (Local)this.Llamadas[i];
+
+                        recaudado += auxiliarLocal.CostoLlamada;
+                    }
+
+                }
+
+                for (i = 0; i < this.Llamadas.Count; i++)
+                {
+                    if (this.Llamadas[i] is Provincial)
+                    {
+                        auxiliarProvincial = (Provincial)this.Llamadas[i];
+
+                        recaudado += auxiliarProvincial.CostoLlamada;
+                    }
+
+                }
+            }
+
+            return recaudado;
+            
+
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder datos = new StringBuilder();
+
+            datos.AppendFormat("Razón Social:{0}\nGanancia Total: {1}\nGanancia Local:{2}\nGanancia Provincial:{3}", this.razonSocial, this.GananciasPorTotal, this.GananciasPorLocal, this.GananciasPorProvincial);
+            datos.AppendLine();
+            datos.AppendLine();
+            datos.AppendLine("Detalle de llamadas:");
+            datos.AppendLine();
+            datos.AppendFormat("{0, 3} {1, 10} {2, 10} {3, 10}\n", "Duracion", "Destino", "Origen", "Costo");
+
+
+
+            foreach (Llamada llamada in this.Llamadas)
+            {
+                datos.AppendFormat("{0}", llamada.Mostrar());
+                datos.AppendLine();
+            }
+
+            return datos.ToString();
+        }
+
+        public void OrdenarLlamadas()
+        {
+            this.Llamadas.Sort(Llamada.OrdenarPorDuracion);
+
+        
+        }
+    }
+}
---
+++ Ejercicios_Guia/08_Herencia/CentralTelefonica/CentralitaHerencia/Local.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Local : Llamada
+    {
+        protected float costo;
+
+        public float CostoLlamada
+        {
+            get
+            {
+                return this.CalcularCosto();
+            }
+        }
+
+        public Local(Llamada llamada, float costo)
+            : base(llamada.Duracion, llamada.NroDestino, llamada.NroOrigen)
+        {
+            this.costo = costo;
+            
+
+        }
+
+        public Local(string origen, float duracion, string destino, float costo)
+            : base(duracion, origen, destino)
+        {
+            this.costo = costo;
+
+        }
+
+        private float CalcularCosto()
+        {
+            float valor;
+
+            valor = this.Duracion * this.costo;
+
+            return valor;
+
+        }
+
+        public override string  Mostrar()
+        {
+            StringBuilder datos = new StringBuilder();
+
+            datos.AppendFormat("{0}", base.Mostrar());
+            datos.AppendFormat(" {0,-10}", this.CostoLlamada);
+
+            return datos.ToString();
+        }
+    }
+}
