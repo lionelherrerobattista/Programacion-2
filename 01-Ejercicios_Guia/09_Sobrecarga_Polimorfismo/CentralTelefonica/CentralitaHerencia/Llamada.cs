e233542fc829aa02eee9d03209fc054a79bbfe63
Lionel Herrero Battista <->
2019-05-13 14:30:21 -0300

    abstract

---
+++ 01-Ejercicios_Guia/09_Sobrecarga_Polimorfismo/CentralTelefonica/CentralitaHerencia/Llamada.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public enum TipoLlamada
+    {
+        Local,
+        Provincial,
+        Todas
+    }
+
+    public abstract class Llamada
+    {
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+
+        public abstract float CostoLlamada
+        {
+            get;
+        }
+
+        public float Duracion
+        {
+            get
+            {
+                return duracion;
+
+            }
+        }
+
+        public string NroDestino
+        {
+            get
+            {
+
+                return this.nroDestino;
+            }
+        }
+
+        public string NroOrigen
+        {
+            get
+            {
+                return this.nroOrigen;
+            }
+        }
+
+        public Llamada(float duracion, string nroDestino, string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;
+
+        }
+
+        protected virtual string Mostrar()
+        {
+            StringBuilder datos = new StringBuilder();
+
+
+            datos.AppendFormat("   {0, -10} {1, -10} {2, -10}", this.Duracion, this.NroDestino,
+                this.NroOrigen);
+
+            return datos.ToString();
+        }
+
+        public static int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            int retorno = 0;
+
+            if (llamada1.Duracion < llamada2.Duracion)
+            {
+                retorno = -1;
+
+            }
+            else if (llamada1.Duracion > llamada2.Duracion)
+            {
+                retorno = 1;
+            }
+
+            return retorno;
+
+        }
+
+        public static bool operator !=(Llamada l1, Llamada l2)
+        {
+
+        }
+
+        public static bool operator ==(Llamada l1, Llamada l2)
+        {
+            //utilizar equals
+        }
+            
+    }
+}
---
+++ 01-Ejercicios_Guia/09_Sobrecarga_Polimorfismo/CentralTelefonica/CentralitaHerencia/Provincial.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+
+
+    public class Provincial : Llamada
+    {
+        public enum Franja
+        {
+            Franja_1,
+            Franja_2,
+            Franja_3
+
+        }
+
+        protected Franja franjaHoraria;
+
+        public override float CostoLlamada
+        {
+            get
+            {
+                return this.CalcularCosto();
+
+            }
+        }
+
+        public Provincial(Franja miFranja, Llamada llamada)
+            : base(llamada.Duracion, llamada.NroDestino, llamada.NroOrigen)
+        {
+            this.franjaHoraria = miFranja;
+
+            
+
+        }
+
+        public Provincial(string origen, Franja miFranja, float duracion, string destino)
+            : base(duracion, destino, origen)
+        {
+            this.franjaHoraria = miFranja;
+            
+
+        }
+
+        private float CalcularCosto()
+        {
+            float costo;
+            
+            if(this.franjaHoraria == Franja.Franja_1)
+            {
+                costo = this.Duracion * (float)0.99;
+
+            }
+            else if(this.franjaHoraria == Franja.Franja_2)
+            {
+                costo = this.Duracion * (float)1.25;
+            }
+            else
+            {
+                costo = this.Duracion * (float)0.66;
+            }
+
+            return costo;
+        }
+
+        protected override string Mostrar()
+        {
+            StringBuilder datos = new StringBuilder();
+
+            datos.AppendFormat("{0}", base.Mostrar());
+            datos.AppendFormat(" {0,-10}", this.CostoLlamada);
+
+            return datos.ToString();
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool esProvincial = false;
+
+            if(obj is Provincial)
+            {
+                esProvincial = true;
+            }
+
+            return esProvincial;
+             
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+
+    }
+}
