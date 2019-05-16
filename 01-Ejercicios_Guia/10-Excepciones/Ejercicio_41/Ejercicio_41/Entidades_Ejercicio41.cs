9f7743044235a5ca5fe05eb112d23ad28e750e50
Lionel <lionherrero@gmail.com>
2019-05-15 22:05:21 -0300

    Excepciones

---
+++ 01-Ejercicios_Guia/10-Excepciones/Ejercicio_41/Ejercicio_41/Entidades_Ejercicio41.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_41
+{
+  public class Divicion
+  {
+    double numeroUno;
+    double numeroDos;
+
+    public double Dividir(double a, double b)
+    {
+      double respuesta;
+
+      try
+      {
+        respuesta = a / b;
+      }
+      catch
+      {
+        throw new
+      }
+      
+    }
+
+  }
+}
