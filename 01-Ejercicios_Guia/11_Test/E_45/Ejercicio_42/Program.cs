7014e67ef7c177beb50a11ac9b65eb02364b193f
Lionel <lionherrero@gmail.com>
2019-05-16 22:05:49 -0300

    Tests Unitarios

---
+++ 01-Ejercicios_Guia/11_Test/E_45/Ejercicio_42/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                OtraClase c = new OtraClase();
+                
+                c.MiMetodoDeInstancia();
+            }
+            catch (MiException e)
+            {
+                Console.WriteLine(e.Message);
+
+                
+
+                if (!(e.InnerException is null))
+                {
+                    Exception excepcion = e.InnerException;
+                   
+                    do
+                    {
+                        Console.WriteLine(excepcion.Message);
+                        excepcion = excepcion.InnerException;
+
+                    } while (!(object.ReferenceEquals(excepcion, null)));
+                }
+                
+
+
+            }
+
+            Console.ReadKey();
+
+            
+            
+
+
+        }
+    }
+}
---
+++ 01-Ejercicios_Guia/11_Test/E_45/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70d25261-bea3-4065-9cee-b6fa6dcb2e94")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
