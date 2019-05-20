c121328ca5d063d0fbe6541653f14dd35e166f7a
Lionel Herrero Battista <->
2019-05-20 15:52:31 -0300

    Tests Unitarios

--- 01-Ejercicios_Guia/10_Excepciones/Ejercicio_43/Ejercicio_36/Program.cs
+++ 01-Ejercicios_Guia/10_Excepciones/Ejercicio_43/Ejercicio_36/Program.cs
@@ -7,71 +7,73 @@ using Entidades_Ejercicio_36;
 
 namespace Ejercicio_36
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      AutoF1 primerAuto = new AutoF1(23, "Ferrari", 200);
-      AutoF1 segundoAuto = new AutoF1(23, "Ferrari", 200);
-      MotoCross primeraMoto = new MotoCross(23, "McClaren", 88);
-      Competencia campeonatoF1 = new Competencia(60, 30, TipoCompetencia.F1);
-
-      Console.Title = "Ejercicio_30";
-
-      //Muestro el auto creado
-      Console.WriteLine("Antes de entrar en competencia:");
-      Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10}", "Número", "Escudería", "Vueltas", "Combustible");
-      Console.WriteLine(primerAuto.MostrarDatos());
-
-      //Cargo un auto a la lista
-      if (campeonatoF1 + primerAuto)
-      {
-        Console.WriteLine("Cargó");
-
-      }
-      else
-      {
-        Console.WriteLine("Error");
-      }
-      try
-      {
-
-        if (campeonatoF1 + primeraMoto)
+        static void Main(string[] args)
         {
-          Console.WriteLine("Cargó");
+            AutoF1 primerAuto = new AutoF1(23, "Ferrari", 200);
+            AutoF1 segundoAuto = new AutoF1(23, "Ferrari", 200);
+            MotoCross primeraMoto = new MotoCross(23, "McClaren", 88);
+            Competencia campeonatoF1 = new Competencia(60, 30, Competencia.TipoCompetencia.F1);
 
+            Console.Title = "Ejercicio_30";
+
+            //Muestro el auto creado
+            Console.WriteLine("Antes de entrar en competencia:");
+            Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10}", "Número", "Escudería", "Vueltas", "Combustible");
+            Console.WriteLine(primerAuto.MostrarDatos());
+
+            //Cargo un auto a la lista
+            if (campeonatoF1 + primerAuto)
+            {
+                Console.WriteLine("Cargó");
+
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+            //No se tienen que cargar
+            try
+            {
+
+                if (campeonatoF1 + primeraMoto)
+                {
+                    Console.WriteLine("Cargó");
+
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(String.Format("\n{0}\n", e.ToString()));
+            }
+
+            /*
+            if (campeonatoF1 + segundoAuto)
+            {
+                Console.WriteLine("Cargó");
+
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }*/
+
+            //Compruebo que esté guardado
+            Console.WriteLine("\nDespués de entrar en competencia:");
+            if (primerAuto.EnCompetencia == true)
+            {
+                Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10}", "Número", "Escudería", "Vueltas", "Combustible");
+                Console.WriteLine(primerAuto.MostrarDatos());
+            }
+
+
+            Console.ReadKey();
         }
-        else
-        {
-          Console.WriteLine("Error");
-        }
-      }
-      catch(CompetenciaNoDisponibleException e)
-      {
-        Console.WriteLine(String.Format("\n{0}\n",e.ToString()));
-      }
-
-
-      if (campeonatoF1 + segundoAuto)
-      {
-        Console.WriteLine("Cargó");
-
-      }
-      else
-      {
-        Console.WriteLine("Error");
-      }
-
-      //Compruebo que esté guardado
-      Console.WriteLine("\nDespués de entrar en competencia:");
-      if (primerAuto.EnCompetencia == true)
-      {
-        Console.WriteLine("{0,-10} {1,-10} {2,-10} {3,-10}", "Número", "Escudería", "Vueltas", "Combustible");
-        Console.WriteLine(primerAuto.MostrarDatos());
-      }
-
-
-      Console.ReadKey();
-      }
     }
-  }
+}
--- 01-Ejercicios_Guia/10_Excepciones/Ejercicio_43/Entidades_Ejercicio_36/Competencia.cs
+++ 01-Ejercicios_Guia/10_Excepciones/Ejercicio_43/Entidades_Ejercicio_36/Competencia.cs
@@ -6,154 +6,200 @@ using System.Threading.Tasks;
 
 namespace Entidades_Ejercicio_36
 {
-  public class Competencia
-  {
-
-    private short cantidadCompetidores;
-    private short cantidadVueltas;
-    private List<VehiculoDeCarrera> competidores;
-    private TipoCompetencia tipo;
-
-    public short CantidadCompetidores
+    public class Competencia
     {
-      get
-      {
-        return this.cantidadCompetidores;
 
-      }
-      set
-      {
-        this.cantidadCompetidores = value;
-
-      }
-    }
-
-    public short CantidadVueltas
-    {
-      get
-      {
-        return this.cantidadVueltas;
-      }
-      set
-      {
-        this.cantidadVueltas = value;
-      }
-    }
-
-    public VehiculoDeCarrera this[int i]
-    {
-      get
-      {
-        return this[i];
-      }
-    }
-
-    public TipoCompetencia Tipo
-    {
-      get
-      {
-        return this.tipo;
-      }
-      set
-      {
-        this.tipo = value;
-      }
-    }
-
-    private Competencia()
-    {
-      this.competidores = new List<VehiculoDeCarrera>();
-    }
-
-    public Competencia(short cantidadVueltas, short cantidadCompetidores, TipoCompetencia tipo) : this()
-    {
-      this.CantidadVueltas = cantidadVueltas;
-      this.CantidadCompetidores = cantidadCompetidores;
-      this.Tipo = tipo;
-    }
-
-    public static bool operator +(Competencia c, VehiculoDeCarrera a)
-    {
-      bool agregoCompetidor = false;
-
-      Random r = new Random();
-
-      try
-      {
-
-        if (c.competidores.Count < c.cantidadCompetidores && c != a)
+        public enum TipoCompetencia
         {
-          if (c.Tipo == TipoCompetencia.MotoCross && a is MotoCross)
-          {
-            c.competidores.Add(a);
-            c.competidores[c.competidores.IndexOf(a)].EnCompetencia = true;
-            c.competidores[c.competidores.IndexOf(a)].VueltasRestantes = c.cantidadVueltas;
-            c.competidores[c.competidores.IndexOf(a)].CantidadCombustible = (short)r.Next(15, 101);
-
-            agregoCompetidor = true;
-
-
-
-          }
-
-          if (c.Tipo == TipoCompetencia.F1 && a is AutoF1)
-          {
-
-            c.competidores.Add(a);
-            c.competidores[c.competidores.IndexOf(a)].EnCompetencia = true;
-            c.competidores[c.competidores.IndexOf(a)].VueltasRestantes = c.cantidadVueltas;
-            c.competidores[c.competidores.IndexOf(a)].CantidadCombustible = (short)r.Next(15, 101);
-
-            agregoCompetidor = true;
-
-
-          }
-
-        }
-      }
-      catch (CompetenciaNoDisponibleException e)
-      {
-        throw new CompetenciaNoDisponibleException("Competencia incorrecta",
-          "VehiculoYaAgregado", "Operator +", e);
-      }
-
-
-      return agregoCompetidor;
-    }
-
-    public static bool operator ==(Competencia c, VehiculoDeCarrera a)
-    {
-
-      int i;
-
-      bool sonIguales = false;
-
-      for (i = 0; i < c.competidores.Count; i++)
-      {
-
-        if (c.competidores[i] == a)
-        {
-          sonIguales = true;
-
-        }
-        else
-        {
-          throw new CompetenciaNoDisponibleException("El vehículo no corresponde a la competencia",
-            "Vehiculo", "operator ==");
-
+            F1,
+            MotoCross
         }
 
-      }
+        private short cantidadCompetidores;
+        private short cantidadVueltas;
+        private List<VehiculoDeCarrera> competidores;
+        private TipoCompetencia tipo;
 
-      return sonIguales;
+        public List<VehiculoDeCarrera> Competidores
+        {
+            get
+            {
+                return this.competidores;
+            }
+        }
+
+        public short CantidadCompetidores
+        {
+            get
+            {
+                return this.cantidadCompetidores;
+
+            }
+            set
+            {
+                this.cantidadCompetidores = value;
+
+            }
+        }
+
+        public short CantidadVueltas
+        {
+            get
+            {
+                return this.cantidadVueltas;
+            }
+            set
+            {
+                this.cantidadVueltas = value;
+            }
+        }
+
+        public VehiculoDeCarrera this[int i]
+        {
+            get
+            {
+                return this.competidores[i];
+            }
+        }
+
+        public TipoCompetencia Tipo
+        {
+            get
+            {
+                return this.tipo;
+            }
+            set
+            {
+                this.tipo = value;
+            }
+        }
+
+        private Competencia()
+        {
+            this.competidores = new List<VehiculoDeCarrera>();
+        }
+
+        public Competencia(short cantidadVueltas, short cantidadCompetidores, TipoCompetencia tipo) : this()
+        {
+            this.CantidadVueltas = cantidadVueltas;
+            this.CantidadCompetidores = cantidadCompetidores;
+            this.Tipo = tipo;
+        }
+
+        public static bool operator +(Competencia c, VehiculoDeCarrera a)
+        {
+            bool agregoCompetidor = false;
+
+            Random r = new Random();
+
+
+            if (c.competidores.Count < c.cantidadCompetidores)
+            {
+                try
+                {
+                    if (c == a)
+                    {
+                        c.competidores.Add(a);
+                        c.competidores[c.competidores.IndexOf(a)].EnCompetencia = true;
+                        c.competidores[c.competidores.IndexOf(a)].VueltasRestantes = c.cantidadVueltas;
+                        c.competidores[c.competidores.IndexOf(a)].CantidadCombustible = (short)r.Next(15, 101);
+
+                        agregoCompetidor = true;
+                    }
+                }
+                catch(CompetenciaNoDisponibleException e)
+                {
+                    throw new CompetenciaNoDisponibleException("Competencia Incorrecta", "Competencia", "Operator +", e);
+                }
+
+
+            }
+
+            return agregoCompetidor;
+        }
+
+        /// <summary>
+        /// Devuelve true si el auto es del mismo tipo
+        /// y no forma parte de la competencia
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static bool operator ==(Competencia c, VehiculoDeCarrera a)
+        {
+            int i;
+            bool noFormaParte = true;
+
+            TipoCompetencia tipo;
+
+            tipo = c.Tipo;
+
+            switch(tipo)
+            {
+                case Competencia.TipoCompetencia.F1:
+                    if( a is AutoF1)
+                    {
+                        for (i = 0; i < c.competidores.Count; i++)
+                        {
+
+                            if (c[i] == a)
+                            {
+                                noFormaParte = false;
+
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        noFormaParte = false;
+
+                    }
+                    break;
+
+                case Competencia.TipoCompetencia.MotoCross:
+                    if (a is MotoCross)
+                    {
+                        for (i = 0; i < c.competidores.Count; i++)
+                        {
+
+                            if (c[i] == a)
+                            {
+                                noFormaParte = false;
+
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        noFormaParte = false;
+
+
+                    }
+                    break;
+
+                default:
+                    noFormaParte = false;
+                    break;
+  
+            }
+
+            if(noFormaParte == false)
+            {
+                throw new CompetenciaNoDisponibleException("El vehículo no corresponde a la competencia",
+                    "Vehiculo", "operator ==");
+            }
+
+            return noFormaParte;
+
+        }
+
+        public static bool operator !=(Competencia c, VehiculoDeCarrera a)
+        {
+            return !(c == a);
+        }
+
 
     }
-
-    public static bool operator !=(Competencia c, VehiculoDeCarrera a)
-    {
-      return !(c == a);
-    }
-
-
-  }
 }
