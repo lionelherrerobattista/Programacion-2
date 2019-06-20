72a75cdec86f434dcddd691256f54d621aabe8f5
Lionel Herrero Battista <->
2019-06-20 17:02:49 -0300

    Métodos de Extensión

---
+++ 01-Ejercicios_Guia/19_Metodos_de_extension/Ejercicio_72/Ejercicio_72/MiExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodoDeExtension
+{
+    public static class MiExtension
+    {
+        public static int CantidadDeDigitos(this long numero)
+        {
+            int cantidadDigitos;
+
+            return cantidadDigitos = numero.ToString().Length;
+
+        }
+    }
+}
---
+++ 01-Ejercicios_Guia/19_Metodos_de_extension/Ejercicio_72/Ejercicio_72/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MetodoDeExtension;
+
+namespace Ejercicio_72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeroString;
+            long numero;
+
+            do
+            {
+                Console.Write("Ingrese un número: ");
+                numeroString = Console.ReadLine();
+
+                if(long.TryParse(numeroString, out numero))
+                {
+                    Console.WriteLine(String.Format("Número de {0} dígito(s)", numero.CantidadDeDigitos()));
+                }
+                else
+                {
+                    Console.WriteLine("Debe ingresar un valor numérico.");
+                    Console.ReadLine();
+                }
+
+
+            } while (numero != 0);
+        }
+    }
+}
