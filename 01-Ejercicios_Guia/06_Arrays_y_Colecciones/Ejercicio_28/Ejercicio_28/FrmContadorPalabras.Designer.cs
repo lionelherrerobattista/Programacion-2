ca45b38a1406f9149140bbef6c8746c911df4876
Lionel Herrero Battista <->
2019-06-20 19:44:40 -0300

    Colecciones

---
+++ 01-Ejercicios_Guia/06_Arrays_y_Colecciones/Ejercicio_28/Ejercicio_28/FrmContadorPalabras.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Ejercicio_28
+{
+    partial class FrmContadorPalabras
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbTexto = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbTexto
+            // 
+            this.rtbTexto.Location = new System.Drawing.Point(0, 0);
+            this.rtbTexto.Name = "rtbTexto";
+            this.rtbTexto.Size = new System.Drawing.Size(431, 380);
+            this.rtbTexto.TabIndex = 0;
+            this.rtbTexto.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(343, 386);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // FrmContadorPalabras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 421);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rtbTexto);
+            this.Name = "FrmContadorPalabras";
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbTexto;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ 01-Ejercicios_Guia/06_Arrays_y_Colecciones/Ejercicio_28/Ejercicio_28/FrmContadorPalabras.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_28
+{
+    public partial class FrmContadorPalabras : Form
+    {
+        private Dictionary<string, int> contadorPalabras;
+
+        public FrmContadorPalabras()
+        {
+            InitializeComponent();
+            this.contadorPalabras = new Dictionary<string, int>();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string texto;
+            string topTres = String.Empty;
+            int contadorTopTres = 0;
+
+            texto = this.rtbTexto.Text;
+            
+            //Separo las palabras
+            string[] palabras = texto.Split(new char[] {'\n', ' ', ',', '.', ';'}, StringSplitOptions.RemoveEmptyEntries);
+
+            //Tomo cada palabra del texto
+            foreach(string palabra in palabras)
+            {
+                //Si está +1 al value
+                if(this.contadorPalabras.ContainsKey(palabra))
+                {
+                    this.contadorPalabras[palabra]++;
+                }
+                else//si no está, agrego la palabra
+                {
+                    this.contadorPalabras.Add(palabra, 1);
+                }
+            }
+
+            //Ordeno de manera descendente y guardo el Top 3 en un string
+            foreach(KeyValuePair<string, int> palabra in this.contadorPalabras.OrderByDescending(key => key.Value))
+            {
+                if(topTres == String.Empty)
+                {
+                    topTres = String.Format("{0} --- {1}", palabra.Key, palabra.Value);
+
+                }
+                else
+                {
+                    topTres = String.Format("{0}\n{1} --- {2}", topTres, palabra.Key, palabra.Value);
+                }
+                contadorTopTres++;
+
+                if(contadorTopTres == 3)
+                {
+                    break;
+                }
+            }
+
+            //Muestro el top 3
+            MessageBox.Show(topTres);
+
+        }
+
+        public int OrdenarDescendente(int a, int b)
+        {
+            int valor;
+
+            if(a < b)
+            {
+                valor = 1;
+            }
+            else
+            {
+                valor = -1;
+            }
+
+            return valor;
+        }
+    }
+}
