f54ea93fb38aba557e60425acd212f92c5cefff5
Lionel Herrero Battista <->
2019-06-20 18:29:15 -0300

    Métodos de Extensión

---
+++ 01-Ejercicios_Guia/19_Metodos_de_extension/Ejercicio_73/Ejercicio_73/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MetodoDeExtensionString;
+
+namespace Ejercicio_73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+
+            do
+            {
+                Console.WriteLine("Ingrese una frase para contar sus signos de puntuación: ");
+
+                palabra = Console.ReadLine();
+
+                Console.WriteLine(String.Format("El texto tiene {0} signos de puntuación", palabra.CantidadSignosPuntuacion()));
+
+
+            } while (palabra != String.Empty);
+        }
+    }
+}
---
+++ 01-Ejercicios_Guia/19_Metodos_de_extension/Ejercicio_73/Ejercicio_73/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodoDeExtensionString
+{
+    public static class StringExtension
+    {
+        public static int CantidadSignosPuntuacion(this string texto)
+        {
+            int contadorSignos = 0;
+
+            foreach(char c in texto)
+            {
+                if(c == '.' || c == ',' || c == ';')
+                {
+                    contadorSignos++;
+                }
+            }
+
+            return contadorSignos;
+        }
+
+    }
+}
