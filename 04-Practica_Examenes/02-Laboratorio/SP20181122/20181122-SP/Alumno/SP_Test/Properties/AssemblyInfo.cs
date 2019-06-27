0c1da6a7b22d859a4ba016528ab9d11945c804a7
Lionel Herrero Battista <->
2019-06-26 23:27:36 -0300

    Practica SP

---
+++ 04-Practica_Examenes/02-Laboratorio/SP20181122/20181122-SP/Alumno/SP_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SP_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SP_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7cadddf2-3503-4f35-a325-62cc04b7553a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 04-Practica_Examenes/02-Laboratorio/SP20181122/20181122-SP/Alumno/SP_Test/TestArchivoTexto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Archivos;
+using Entidades;
+using System.Collections.Generic;
+
+namespace SP_Test
+{
+    [TestClass]
+    public class TestArchivoTexto
+    {
+        [TestMethod]
+        public void GuardarTexto()
+        {
+            //Arrange
+            Texto archivoTexto = new Texto();
+            Patente p1 = new Patente("SUI302", Patente.Tipo.Vieja);
+
+            Queue<Patente> cola = new Queue<Patente>();
+            Queue<Patente> cola2 = new Queue<Patente>();
+
+            cola.Enqueue(p1);
+            try
+            {
+                archivoTexto.Guardar("archivoTest.txt", cola);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+
+            archivoTexto.Leer("archivoTest.txt", out cola2);
+
+            Assert.AreEqual(cola.Dequeue().CodigoPatente, cola2.Dequeue().CodigoPatente);
+
+        }
+    }
+}
